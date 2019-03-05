d69384b57010bad3aa30a61370019ca45e08ac6e
Hamster-Furtif <lakartoonerie_dutronc@yahoo.fr>
2019-03-05 12:18:47 +0100

    Ajout des fonctions dans le body de opstrat

--- src/opstrat.adb
+++ src/opstrat.adb
@@ -42,11 +42,96 @@ begin
       if(Get_Winning_Chance(op_hand.set(0), op_hand.set(1)) < 0.5 and getBet(history, 0) > 0) then
          r := 1.0;
       elsif(Get_Winning_Chance(op_hand.set(0), op_hand.set(1)) < 0.75 and getBet(history, 0) > 0) then
-         r := 3.0 - Get_Winning_Chance(op_hand.set(0), op_hand.set(1)*4;
+         r := 3.0 - Get_Winning_Chance(op_hand.set(0), op_hand.set(1))*4;
       else
          r := 0.0;
       end if;
       
       return r;
    end opIsBluffing;
+   
+   procedure add_bluff(logic : in out T_logic; r : in Float) is
+      r : Float;
+   begin
+      return r;
+   end add_bluff;
+   
+   procedure add_semi_bluff(logic : in out T_logic; r : in Float) is
+      r : Float;
+   begin
+      return r;
+   end add_semi_bluff;
+   
+   procedure add_bluffed(logic : in out T_logic; r : in Float) is
+      r : Float;
+   begin
+      return r;
+   end add_bluffed;
+   
+   procedure add_chances(logic : in out T_logic; chances : in Float) is
+      r : Float;
+   begin
+      return r;
+   end add_chances;
+   
+   function get_avg_chances(logic : T_logic) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_avg_chances;
+   
+   function get_min_chances(logic : T_logic) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_min_chances;
+   
+   function get_can_bluff(logic : T_logic) return Boolean is
+      r : Boolean;
+   begin
+      return r;
+   end get_can_bluff;
+   
+   function get_can_semi_bluff(logic : T_logic) return Boolean is
+      r : Boolean;
+   begin
+      return r;
+   end get_can_semi_bluff;
+   
+   function get_can_get_bluffed(logic : T_logic) return Boolean is
+      r : Boolean;
+   begin
+      return r;
+   end get_can_get_bluffed;
+   
+   function get_has_logic(logic : T_logic) return Boolean is
+      r : Boolean;
+   begin
+      return r;
+   end get_has_logic;
+   
+   function get_winning_chances(game : T_game) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_winning_chances;
+   
+   function get_nbr_of_bluff(logic : T_logic) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_nbr_of_bluff;
+   
+   function get_nbr_of_semi_bluff(logic : T_logic) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_nbr_of_semi_bluff;
+   
+   function get_nbr_of_bluffed(logic : T_logic) return Float is
+      r : Float;
+   begin
+      return r;
+   end get_nbr_of_bluffed;
+   
 end opstrat;
